bbf7c0f7c7fd39082ad375542b007c122853c586
bpol <wojciechdanowskiz@gmail.com>
2020-06-09 15:28:00 +0200

    removed console log

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,9 +4,9 @@ import Users from "./Users";
 import { deleteUser } from "../services/users";
 const UsersList = () => {
   const dispatch = useDispatch();
+
   const handleDeleteUser = (id) => {
     dispatch(deleteUser(id));
-    console.log("kliknięto z propsa");
   };
 
   return (
